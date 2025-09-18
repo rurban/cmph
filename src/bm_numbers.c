28842b35c30c24b7f1322c9782aa838ef6004d71
Reini Urban <reini.urban@gmail.com>
2025-09-18 12:41:43 +0200

    bm: combine bm_numbers with compiled
    to ensure the same framework, and get comparable numbers

--- src/bm_numbers.c
+++ src/bm_numbers.c
@@ -1,13 +1,20 @@
-/* benchmark all algos for creation and run-time search time costs */
-/* with all hash variants also */
+/* Benchmark all algos and hashes.
+   With COMPILED dlopening the compiled search function */
+/* Added by Reini Urban, 2025 */
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#ifdef COMPILED
+#include <dlfcn.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#endif
 
 #include "config.h"
 #include "bitbool.h"
 #include "cmph.h"
 #include "cmph_benchmark.h"
+#include "debug.h"
 #include "linear_string_map.h"
 
 // Generates a vector with random unique 32 bits integers
@@ -39,6 +46,15 @@ char* create_lsmap_key(CMPH_ALGO algo, CMPH_HASH hash, int iters) {
   return strdup(mphf_name);
 }
 
+#ifdef COMPILED
+long file_size_kb(const char *path)
+{
+    struct stat st;
+    if (stat(path, &st) != 0) return -1;      /* errno set by stat */
+    return st.st_size / 1024;                 /* integer KB */
+}
+#endif
+
 static cmph_uint32 g_numbers_len = 0;
 static cmph_uint32 *g_numbers = NULL;
 static lsmap_t *g_created_mphf = NULL;
@@ -51,6 +67,16 @@ int bm_create(CMPH_ALGO algo, CMPH_HASH *hashes, int iters) {
   cmph_t* mphf = NULL;
   cmph_uint32 nhashes = 0;
   CMPH_HASH hash = hashes ? hashes[0] : 0;
+#ifdef COMPILED
+  char c_file[24];
+  char cmd[256];
+  int result;
+#endif
+#ifdef HAVE_CRC32_HW
+  const char hw[] = "HW ";
+#else
+  const char hw[] = "SW ";
+#endif
 
   if (iters > (int)g_numbers_len) {
     fprintf(stderr, "No input with proper size.");
@@ -67,15 +93,13 @@ int bm_create(CMPH_ALGO algo, CMPH_HASH *hashes, int iters) {
   config = cmph_config_new(source);
   cmph_config_set_algo(config, algo);
   if (nhashes) cmph_config_set_hashfuncs(config, hashes);
+#ifdef COMPILED
+  snprintf(c_file, sizeof(c_file), "bm_%s_%s.c", cmph_names[algo], cmph_hash_names[hash]);
+#endif
 
   cmph_config_set_graphsize(config, 5);
   mphf = cmph_new(config);
   if (!mphf) {
-#ifdef HAVE_CRC32_HW
-    const char hw[] = "HW ";
-#else
-    const char hw[] = "SW ";
-#endif
     fprintf(stderr, "Failed to create mphf for algorithm %s with %shash %s and %u keys\n",
             cmph_names[algo], hash == CMPH_HASH_CRC32 ? hw : "",
             cmph_hash_names[hash], iters);
@@ -84,6 +108,37 @@ int bm_create(CMPH_ALGO algo, CMPH_HASH *hashes, int iters) {
     //g_created_mphf = NULL;
     return 1;
   }
+#ifdef COMPILED
+  DEBUGP("cmph_compile(mphf, config, \"%s\", \"<>\")\n", c_file);
+  result = cmph_compile(mphf, config, c_file, "<>");
+  if (!result) {
+    fprintf(stderr, "Failed to compile mphf for algorithm %s with %shash %s\n",
+            cmph_names[algo], hash == CMPH_HASH_CRC32 ? hw : "",
+            cmph_hash_names[hash]);
+    cmph_config_destroy(config);
+    cmph_io_struct_vector_adapter_destroy(source);
+    return 1;
+  }
+  long c_sz = file_size_kb(c_file);
+  printf("Created %s: %lu KB\n", c_file, c_sz);
+#if defined(__GNUC__) && defined(__x86_64__)
+  #define OPTIMS "-march=native"
+#else
+  #define OPTIMS ""
+#endif
+  snprintf(cmd, sizeof(cmd),
+           "cc -shared -fPIC %s %s -g -I\"%s/src\" -o bm_%s_%s.so %s",
+           c_sz > 2000 ? "-O0" : "-O2", OPTIMS, top_srcdir, cmph_names[algo],
+           cmph_hash_names[hash], c_file);
+  DEBUGP("%s\n", cmd);
+  if (system(cmd)) {
+    fprintf(stderr, "Failed to run %s\n", cmd);
+    cmph_config_destroy(config);
+    cmph_io_struct_vector_adapter_destroy(source);
+    return 1;
+  }
+#endif
+
   cmph_config_destroy(config);
   cmph_io_struct_vector_adapter_destroy(source);
   lsmap_append(g_created_mphf, create_lsmap_key(algo, hash, iters), mphf);
@@ -101,9 +156,29 @@ int bm_search(CMPH_ALGO algo, CMPH_HASH *hashes, int iters) {
   mphf = (cmph_t*)lsmap_search(g_created_mphf, mphf_name);
   free(mphf_name);
 
-  if (!mphf) {
+  if (!mphf)
+    return 1;
+#ifdef COMPILED
+  char so_file[80];
+  snprintf(so_file, sizeof(so_file), "./bm_%s_%s.so", cmph_names[algo],
+           cmph_hash_names[hash]);
+  DEBUGP("dlopen \"%s\"\n", so_file);
+  void* dl = dlopen(so_file, RTLD_NOW | RTLD_GLOBAL);
+  if (!dl) {
+    fprintf(stderr, "Failed to dlopen %s\n", so_file);
+    //unlink(so_file);
     return 1;
   }
+  DEBUGP("cmph_c_search\n");
+  uint32_t (*search_fn)(const char*,uint32_t) =
+    (uint32_t (*)(const char*,uint32_t))dlsym(dl, "cmph_c_search");
+  if (!search_fn) {
+    fprintf(stderr, "Failed to find symbol cmph_c_search in %s\n", so_file);
+    dlclose(dl);
+    //unlink(so_file);
+    return 1;
+  }
+#endif
 
   cmph_uint32* count = (cmph_uint32*)calloc(iters, sizeof(cmph_uint32));
   cmph_uint32 size = cmph_size(mphf);
@@ -112,7 +187,11 @@ int bm_search(CMPH_ALGO algo, CMPH_HASH *hashes, int iters) {
   for (i = 0; i < iters * 100; ++i) {
     cmph_uint32 pos = random() % iters;
     const char* buf = (const char*)(g_numbers + pos);
+#ifdef COMPILED
+    cmph_uint32 h = search_fn(buf, sizeof(cmph_uint32));
+#else
     cmph_uint32 h = cmph_search(mphf, buf, sizeof(cmph_uint32));
+#endif
     ++count[pos];
     assert(h < size && "h out of bounds");
     ++hash_count[h];
@@ -121,6 +200,10 @@ int bm_search(CMPH_ALGO algo, CMPH_HASH *hashes, int iters) {
   // Verify correctness later. NYI
   lsmap_append(g_expected_probes, create_lsmap_key(algo, hash, iters), count);
   lsmap_append(g_mphf_probes, create_lsmap_key(algo, hash, iters), hash_count);
+#ifdef COMPILED
+  dlclose(dl);
+  unlink(so_file);
+#endif
   return 0;
 }
 
@@ -210,7 +293,7 @@ int main(int argc, char **argv) {
       strncpy(algo, argv[++i], sizeof(algo)-1);
     else if (strcmp(argv[i], "-h") == 0)  // all algos for this hash
       strncpy(hash, argv[++i], sizeof(algo)-1);
-    else if (strcmp(argv[i], "-s") == 0) // size
+    else if (strcmp(argv[i], "-s") == 0) // size (number of keys)
       g_numbers_len = (cmph_uint32)strtol(argv[++i], NULL, 10);
     else if (strcmp(argv[i], "-i") == 0) // iters
       iters = (cmph_uint32)strtol(argv[++i], NULL, 10);
@@ -250,6 +333,7 @@ int main(int argc, char **argv) {
       BM_REGISTER(bm_create_CMPH_BDZ_PH, iters);
       BM_REGISTER(bm_search_CMPH_BDZ_PH, iters);
     }
+#ifndef COMPILED
     if (!*algo || strcmp(algo, "chd") == 0) {
       BM_REGISTER(bm_create_CMPH_CHD, iters);
       BM_REGISTER(bm_search_CMPH_CHD, iters);
@@ -263,6 +347,7 @@ int main(int argc, char **argv) {
       BM_REGISTER(bm_create_CMPH_BRZ, iters);
       BM_REGISTER(bm_search_CMPH_BRZ, iters);
     }
+#endif
     if (!*algo || strcmp(algo, "bmz8") == 0) {
       //BM_REGISTER(bm_create_CMPH_BMZ8, 255); // only really needed for BRZ subgraphs
       //BM_REGISTER(bm_search_CMPH_BMZ8, 255);
@@ -290,6 +375,7 @@ int main(int argc, char **argv) {
       BM_REGISTER(bm_create_CMPH_BDZ_PH_WYHASH, iters);
       BM_REGISTER(bm_search_CMPH_BDZ_PH_WYHASH, iters);
     }
+#ifndef COMPILED
     if (!*algo || strcmp(algo, "chd") == 0) {
       BM_REGISTER(bm_create_CMPH_CHD_WYHASH, iters);
       BM_REGISTER(bm_search_CMPH_CHD_WYHASH, iters);
@@ -298,6 +384,12 @@ int main(int argc, char **argv) {
       BM_REGISTER(bm_create_CMPH_CHD_PH_WYHASH, iters);
       BM_REGISTER(bm_search_CMPH_CHD_PH_WYHASH, iters);
     }
+    if (!*algo || strcmp(algo, "brz") == 0) {
+      // instable:
+      BM_REGISTER(bm_create_CMPH_BRZ_WYHASH, iters);
+      BM_REGISTER(bm_search_CMPH_BRZ_WYHASH, iters);
+    }
+#endif
   }
 
   if (!*hash || strcmp(hash, "djb2") == 0) {
@@ -323,6 +415,7 @@ int main(int argc, char **argv) {
       BM_REGISTER(bm_create_CMPH_BDZ_PH_DJB2, iters);
       BM_REGISTER(bm_search_CMPH_BDZ_PH_DJB2, iters);
     }
+#ifndef COMPILED
     if (strcmp(algo, "chd") == 0) {
       // unstable
       BM_REGISTER(bm_create_CMPH_CHD_DJB2, iters);
@@ -333,6 +426,12 @@ int main(int argc, char **argv) {
       BM_REGISTER(bm_create_CMPH_CHD_PH_DJB2, iters);
       BM_REGISTER(bm_search_CMPH_CHD_PH_DJB2, iters);
     }
+    if (!*algo || strcmp(algo, "brz") == 0) {
+      // instable:
+      BM_REGISTER(bm_create_CMPH_BRZ_DJB2, iters);
+      BM_REGISTER(bm_search_CMPH_BRZ_DJB2, iters);
+    }
+#endif
   }
 
   if (!*hash || strcmp(hash, "fnv") == 0) {
@@ -356,6 +455,7 @@ int main(int argc, char **argv) {
       BM_REGISTER(bm_create_CMPH_BDZ_PH_FNV, iters);
       BM_REGISTER(bm_search_CMPH_BDZ_PH_FNV, iters);
     }
+#ifndef COMPILED
     if (!*algo || strcmp(algo, "chd") == 0) {
       BM_REGISTER(bm_create_CMPH_CHD_FNV, iters);
       BM_REGISTER(bm_search_CMPH_CHD_FNV, iters);
@@ -364,6 +464,12 @@ int main(int argc, char **argv) {
       BM_REGISTER(bm_create_CMPH_CHD_PH_FNV, iters);
       BM_REGISTER(bm_search_CMPH_CHD_PH_FNV, iters);
     }
+    if (!*algo || strcmp(algo, "brz") == 0) {
+      // instable:
+      BM_REGISTER(bm_create_CMPH_BRZ_FNV, iters);
+      BM_REGISTER(bm_search_CMPH_BRZ_FNV, iters);
+    }
+#endif
   }
 
   if (!*hash || strcmp(hash, "sdbm") == 0) {
@@ -389,6 +495,7 @@ int main(int argc, char **argv) {
       BM_REGISTER(bm_create_CMPH_BDZ_PH_SDBM, iters);
       BM_REGISTER(bm_search_CMPH_BDZ_PH_SDBM, iters);
     }
+#ifndef COMPILED
     if (strcmp(algo, "chd") == 0) {
       // unstable
       BM_REGISTER(bm_create_CMPH_CHD_SDBM, iters);
@@ -399,6 +506,12 @@ int main(int argc, char **argv) {
       BM_REGISTER(bm_create_CMPH_CHD_PH_SDBM, iters);
       BM_REGISTER(bm_search_CMPH_CHD_PH_SDBM, iters);
     }
+    if (!*algo || strcmp(algo, "brz") == 0) {
+      // instable:
+      BM_REGISTER(bm_create_CMPH_BRZ_SDBM, iters);
+      BM_REGISTER(bm_search_CMPH_BRZ_SDBM, iters);
+    }
+#endif
   }
 
   if (!*hash || strcmp(hash, "crc32") == 0) {
@@ -422,6 +535,7 @@ int main(int argc, char **argv) {
       BM_REGISTER(bm_create_CMPH_BDZ_PH_CRC32, iters);
       BM_REGISTER(bm_search_CMPH_BDZ_PH_CRC32, iters);
     }
+#ifndef COMPILED
     if (!*algo || strcmp(algo, "chd") == 0) {
       BM_REGISTER(bm_create_CMPH_CHD_CRC32, iters);
       BM_REGISTER(bm_search_CMPH_CHD_CRC32, iters);
@@ -430,6 +544,12 @@ int main(int argc, char **argv) {
       BM_REGISTER(bm_create_CMPH_CHD_PH_CRC32, iters);
       BM_REGISTER(bm_search_CMPH_CHD_PH_CRC32, iters);
     }
+    if (!*algo || strcmp(algo, "brz") == 0) {
+      // instable:
+      BM_REGISTER(bm_create_CMPH_BRZ_CRC32, iters);
+      BM_REGISTER(bm_search_CMPH_BRZ_CRC32, iters);
+    }
+#endif
   }
 
   run_benchmarks();
