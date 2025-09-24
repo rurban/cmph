1080102a1d4e12e19298f9d6287d8ef437e0d692
Reini Urban <reini.urban@gmail.com>
2025-09-24 20:07:59 +0200

    extend compile tests for keys
    check mult. bins

--- tests/cmph_compile_run.c
+++ tests/cmph_compile_run.c
@@ -7,8 +7,8 @@ uint32_t cmph_c_search(const char* key, uint32_t keylen);
 uint32_t cmph_c_size(void);
 
 int main(int argc, char **argv) {
-    if (argc != 4) {
-        fprintf(stderr, "Usage: ./cmph_compile_run algo words nkeys\n");
+    if (argc < 3) {
+        fprintf(stderr, "Usage: ./cmph_compile_run algo words [nkeys]\n");
         exit(1);
     }
     FILE *f = fopen(argv[2], "r");
@@ -16,16 +16,20 @@ int main(int argc, char **argv) {
         perror("fopen");
         exit(1);
     }
-    uint32_t nkeys = strtol(argv[3], NULL, 10);
-    printf("cmph_compile_run %s for %s [%u]\n", argv[1], argv[2], nkeys);
     int is_order_preserving = strcmp(argv[1], "chm") == 0;
+    // some funcs over-allocate. we need an exported cmph_c_size()
+    uint32_t nkeys = cmph_c_size();
     char key[128];
     unsigned l = 0;
     unsigned failed = 0;
     uint32_t h;
-    // some funcs over-allocate. we need an exported cmph_c_size()
-    nkeys = cmph_c_size();
+    uint32_t keys_per_bin = 1;
     uint32_t *hasharray = (uint32_t*)calloc(nkeys, 4);
+    uint8_t *hashtable;
+    printf("cmph_compile_run %s for %s [%u]\n", argv[1], argv[2], nkeys);
+    hashtable = (uint8_t*)calloc(nkeys, 1);
+    memset(hashtable, 0,(size_t) nkeys);
+    
     while (fgets(key, sizeof(key), f)) {
         // delete the ending \n
         int len = (int)strlen(key);
@@ -39,18 +43,31 @@ int main(int argc, char **argv) {
         } else if (hasharray[h]) {
             fprintf(stderr, "Duplicated or unknown key %*s in the input\n", len-1, key);
             failed++;
-        } else
+        } else if (hashtable[h] >= keys_per_bin) {
+            fprintf(stderr, "More than %u keys were mapped to bin %u\n", keys_per_bin, h);
+            fprintf(stderr, "Duplicated or unknown key %*s in the input\n", len-1, key);
+            failed++;
+        }
+        else {
             hasharray[h] = 1;
+            hashtable[h]++;
+        }
         if (is_order_preserving && h != l) {
             if (failed < 10)
                 printf("%*s => %u: FAIL should be at index %u\n", len-1, key, h, l);
             failed++;
         }
         l++;
+        if (l > nkeys) {
+            fprintf(stderr, "Too many keys in the input, cmph_c_size() was %u\n", nkeys);
+            failed++;
+            break;
+        }
         if (failed > 5)
             break;
     }
     free(hasharray);
+    free(hashtable);
     fclose(f);
     if (failed)
         return 1;
